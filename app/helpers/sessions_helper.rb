efc3aa31f63eb113135e957ff4ad4432ceea89cc
Dai Osada <dai.osada@gmail.com>
2020-06-16 14:37:37 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,7 +25,7 @@ module SessionsHelper
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(:id, user_id)
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
